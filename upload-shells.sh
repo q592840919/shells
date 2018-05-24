f32458f645fb9db3ee91694d0250bf13ecf700c9
q592840919 <xuefengmsa@163.com>
2018-05-24 16:32:38 +0800

    2018-05-24 16:32:38

--- upload-shells.sh
+++ upload-shells.sh
@@ -7,6 +7,7 @@ git add -A
 git commit -m "$v_time"
 git push origin master
 echo "success push git!"
+echo "10s,sometimes internet slow"
 sleep 10s
 ssh root@47.95.215.144 "sh /shells/rebase-shells.sh"
 "echo gone"
