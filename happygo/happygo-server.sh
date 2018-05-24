1a36a9a8e8b0c0339b41be458d7700d6dce8fc00
q592840919 <xuefengmsa@163.com>
2018-05-24 17:58:39 +0800

    2018-05-24 17:58:39

--- happygo/happygo-server.sh
+++ happygo/happygo-server.sh
@@ -2,5 +2,5 @@ cd /minipro/happygo-egg
 git fetch origin
 git rebase origin/master
 docker build -t happygo-egg-server .
-cd /docker/compose/composefile/happygo-egg-server
+cd /docker/composefile/happygo-egg-server
 docker-compose restart
