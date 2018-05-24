92894e3ca060d683ca0d9c7b04c3585a7d4a02fb
q592840919 <xuefengmsa@163.com>
2018-05-24 15:34:16 +0800

    2018-05-24 15:34:16

--- happygo/happygo-server.sh
+++ happygo/happygo-server.sh
@@ -1,3 +1,6 @@
-cd /docker
+cd /minipro/happygo-egg
 git fetch origin
 git rebase origin/master
+docker build -t happygo-egg-server .
+cd /docker/compose/composefile/happygo-egg-server
+docker-compose restart
--- upload-shells.sh
+++ upload-shells.sh
@@ -1,7 +1,8 @@
-echo "uploading to pre: root@47.95.215.144";
+echo "push this change";
 v_time=`date "+%Y-%m-%d %H:%M:%S"`;
 echo $v_time
 git status
 git add -A
 git commit -m "$v_time"
 git push origin master
+echo "success!"
